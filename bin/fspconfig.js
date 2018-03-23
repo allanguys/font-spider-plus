78b6c224daabbb88ad8e692cb08d562281e3da40
Allan <allan_guys@qq.com>
2018-03-23 16:05:11 +0800

    commander init file

---
+++ bin/fspconfig.js
@@ -0,0 +1,9 @@
+{
+    "phantomjs" : "phantomjs",
+    "localPath" : "../font/",
+    "onlinePath" : "../font/",
+    "url" :  [
+    "http://fn.qq.com/web201803/main.shtml",
+    "http://fn.qq.com/web201803/video_list.shtml"
+     ]
+}
